186928e2a170bdc50ca2551b35604ecff58d76ea
Maul <mprambadi@gmail.com>
2019-08-29 13:24:41 +0700

    update message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Vue in Docker Day 4"/>
+    <HelloWorld msg="Welcome Vue in Docker Day 4 Update With Github CI"/>
   </div>
 </template>
 
